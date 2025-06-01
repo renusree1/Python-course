edb8cbcc805468ba47bd439e2365a259f82aebbd
renusree1 <renusree10.n@gmail.com>
2025-06-01 12:42:10 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+print("hello world")
+print("hello","world")
+print("add",1+2)
+print("hello\n","world")
+print("python",end="programing")
+print("hello")
+#print is a inbuilt
+
+#variable
+#syntax-variable name=data
+a=5
+b="renusree"
+c=2
+print("a=",a)
+print(b)
+print(a+c)
+#user input
+name=input("enter your name-")
+print(name)
+number=input("enter your age- ")
+print(number)
+print(name,"your age is",number)
+#keywords- reserved word that has special meanings
+import keyword
+print("python keyword\n")
+print(keyword.kwlist)
